538c07d30064c5ce87482eefcb9b2ee2033b6d90
Bhavan Kumar Velladurai <vlbi111v@mailhub.rrze.uni-erlangen.de>
2026-06-28 18:10:07 +0200

    reproducibility fix

--- main.py
+++ main.py
@@ -49,6 +49,10 @@ def seed_all_random_number_generators(seed):
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
 
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark     = False
+    torch.use_deterministic_algorithms(True, warn_only=True)
+
 
 def parse_command_line_arguments():
     argument_parser = argparse.ArgumentParser(description="MicroUNet experiment runner")
